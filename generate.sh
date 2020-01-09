fde190939cfe01eb6ae3d4da92e74303c29c7e02
Alexej Kloos <alexejkloos@gmail.com>
2020-01-09 09:42:43 +0100

    wip

--- generate.sh
+++ generate.sh
@@ -1,3 +1,8 @@
+# GET NEW FILE
+# http://shopware.test/api/v1/_info/swagger.html
+# wget http://shopware.test/api/v1/_info/openapi3.json?format=yaml
+wget http://shopware.test/api/v1/_info/openapi3.json
+
 swagger-codegen generate -i shopware-openapi3.json -l php --git-user-id industryarena --git-repo-id ShopwareSDK6 --invoker-package ShopwareSDK6
 
 rm -rf docs lib test
